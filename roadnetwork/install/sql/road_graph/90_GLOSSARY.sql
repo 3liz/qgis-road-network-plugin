8bc46895e8e0b9b575ab5b296d4e7488779da5c1
Michael Douchin <mdouchin@3liz.com>
2025-10-15 12:30:15 +0200

    Setup SQL file for the road plugin

--- roadnetwork/install/sql/road_graph/90_GLOSSARY.sql
+++ roadnetwork/install/sql/road_graph/90_GLOSSARY.sql
@@ -20,18 +20,23 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Data for Name: glossary_test_category; Type: TABLE DATA; Schema: road_graph; Owner: -
+-- Data for Name: glossary_road_class; Type: TABLE DATA; Schema: road_graph; Owner: -
 --
 
-INSERT INTO road_graph.glossary_test_category (id, code, label) VALUES (1, 'A', 'Category A');
-INSERT INTO road_graph.glossary_test_category (id, code, label) VALUES (2, 'B', 'Category B');
+INSERT INTO road_graph.glossary_road_class (id, code, label) VALUES (1, 'Intercommunale', 'Intercommunale');
+INSERT INTO road_graph.glossary_road_class (id, code, label) VALUES (2, 'Autoroute', 'Autoroute');
+INSERT INTO road_graph.glossary_road_class (id, code, label) VALUES (3, 'Nationale', 'Nationale');
+INSERT INTO road_graph.glossary_road_class (id, code, label) VALUES (4, 'Communale', 'Communale');
+INSERT INTO road_graph.glossary_road_class (id, code, label) VALUES (5, 'Autre', 'Autre');
+INSERT INTO road_graph.glossary_road_class (id, code, label) VALUES (6, 'Voie privée', 'Voie privée');
+INSERT INTO road_graph.glossary_road_class (id, code, label) VALUES (7, 'Départementale', 'Départementale');
 
 
 --
--- Name: glossary_test_category_id_seq; Type: SEQUENCE SET; Schema: road_graph; Owner: -
+-- Name: glossary_road_scale_id_seq; Type: SEQUENCE SET; Schema: road_graph; Owner: -
 --
 
-SELECT pg_catalog.setval('road_graph.glossary_test_category_id_seq', 2, true);
+SELECT pg_catalog.setval('road_graph.glossary_road_scale_id_seq', 7, true);
 
 
 --
