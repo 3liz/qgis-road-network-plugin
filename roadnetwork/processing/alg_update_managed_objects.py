47b4a8023012b15f8ed0f4df610c8fbb6f171855
Michael Douchin <mdouchin@3liz.com>
2026-07-09 16:19:29 +0200

    Processing alg - update managed objects : add option to preserve start&end positions

--- roadnetwork/processing/alg_update_managed_objects.py
+++ roadnetwork/processing/alg_update_managed_objects.py
@@ -35,6 +35,11 @@ class UpdateManagedObjects(BaseProcessingAlgorithm):
         "update_geom",
         "update_references",
     )
+    PRESERVE_START_END_POSITIONS = "PRESERVE_START_END_POSITIONS"
+    PRESERVE_START_END_POSITIONS_VALUES = (
+        "no",
+        "yes",
+    )
 
     def name(self):
         return "update_managed_objects"
@@ -118,6 +123,19 @@ class UpdateManagedObjects(BaseProcessingAlgorithm):
             )
         )
 
+        # Update geometry : do we need to update references first ?
+        self.addParameter(
+            QgsProcessingParameterEnum(
+                self.PRESERVE_START_END_POSITIONS,
+                tr("Update geometries - Preserve start & end points when updating line geometries"),
+                options=[tr("No"), tr("Yes")],
+                defaultValue=0,
+                allowMultiple=False,
+                optional=False,
+                usesStaticStrings=False,
+            )
+        )
+
         # Output layer
         self.addParameter(
             QgsProcessingParameterFeatureSink(
@@ -192,7 +210,6 @@ class UpdateManagedObjects(BaseProcessingAlgorithm):
         """
         Get the reference of a QGIS feature from the database
         """
-        # print(tr(f"Requesting database for reference of point with coordinates : {longitude}, {latitude}"))
         sql_road_code = "NULL"
         if road_code:
             sql_road_code = f"'{road_code}'"
@@ -205,7 +222,6 @@ class UpdateManagedObjects(BaseProcessingAlgorithm):
             )::json AS ref
         ;
         """
-        # print(sql)
         try:
             data = connection.executeSql(sql)
         except QgsProviderConnectionException as e:
@@ -224,7 +240,6 @@ class UpdateManagedObjects(BaseProcessingAlgorithm):
         """
         Get the geometry of a feature from the database from its reference
         """
-        # print(tr(f"Requesting database for WKT of feature with reference : {references}"))
         if geometry_type == "point":
             sql = f"""
             WITH get_geom AS (
@@ -249,17 +264,32 @@ class UpdateManagedObjects(BaseProcessingAlgorithm):
             FROM get_geom
             """
         else:
+            # We need to invert start and end references if end is lower than start
+            updated_references = {}
+            for ref, value in references.items():
+                updated_references[ref] = value
+                # we calculate the start and end marker + abscissa values
+                # we must convert the QVariant to float
+                start_value = references["start_marker_code"] * 1000000 + references["start_abscissa"]
+                end_value = references["end_marker_code"] * 1000000 + references["end_abscissa"]
+                # we invert them if start is above end
+                if start_value > end_value:
+                    updated_references["start_marker_code"] = references["end_marker_code"]
+                    updated_references["start_abscissa"] = references["end_abscissa"]
+                    updated_references["end_marker_code"] = references["start_marker_code"]
+                    updated_references["end_abscissa"] = references["start_abscissa"]
+
             sql = f"""
             WITH get_geom AS (
                 SELECT
                 road_graph.get_road_substring_from_references(
-                    '{references["road_code"]}',
-                    {references["start_marker_code"]},
-                    {references["start_abscissa"]},
-                    {references["end_marker_code"]},
-                    {references["end_abscissa"]},
-                    {references["offset"]},
-                    '{references["side"]}'
+                    '{updated_references["road_code"]}',
+                    {updated_references["start_marker_code"]},
+                    {updated_references["start_abscissa"]},
+                    {updated_references["end_marker_code"]},
+                    {updated_references["end_abscissa"]},
+                    {updated_references["offset"]},
+                    '{updated_references["side"]}'
                 )->'geom' AS geom
             )
             SELECT
@@ -302,6 +332,13 @@ class UpdateManagedObjects(BaseProcessingAlgorithm):
         update_policy_index = self.parameterAsEnum(parameters, self.UPDATE_POLICY, context)
         update_policy = self.UPDATE_POLICY_VALUES[update_policy_index]
         feedback.pushInfo(tr(f"* Update policy : {update_policy}"))
+        feedback.pushInfo("")
+
+        # Get preserve references parameter
+        feedback.pushInfo(tr("Get parameter preserve positions..."))
+        p_index = self.parameterAsEnum(parameters, self.PRESERVE_START_END_POSITIONS, context)
+        preserve_start_end_positions = self.PRESERVE_START_END_POSITIONS_VALUES[p_index]
+        feedback.pushInfo(tr(f"* Preserve start & end positions : {preserve_start_end_positions}"))
         feedback.pushInfo("")
 
         # Check for cancellation
@@ -348,47 +385,13 @@ class UpdateManagedObjects(BaseProcessingAlgorithm):
 
             # feedback.pushInfo(tr(f"Feature {idx}"))
 
-            # Request the database for the updated references
-            # Set the attributes or the geometry based on the update policy
-            if update_policy == "update_geom":
-                # We need to update the feature geometry from the feature references
-                # Request the database for the updated geometry
-                if QgsWkbTypes.geometryType(source.wkbType()) == QgsWkbTypes.PointGeometry:
-                    references_keys = ["road_code", "marker_code", "abscissa", "side", "offset"]
-                    references = {a: feature[a] for a in references_keys if a in feature.fields().names()}
-                else:
-                    references_keys = [
-                        "road_code",
-                        "start_marker_code",
-                        "start_abscissa",
-                        "end_marker_code",
-                        "end_abscissa",
-                        "side",
-                        "offset",
-                    ]
-                    references = {a: feature[a] for a in references_keys if a in feature.fields().names()}
-
-                # Set default values for side and offset if they are not present in the feature attributes
-                if "side" not in references:
-                    references["side"] = "right"
-                if "offset" not in references:
-                    references["offset"] = 0
-
-                # Get WKT geometry from the database
-                geometry_type = "point"
-
-                if QgsWkbTypes.geometryType(source.wkbType()) == QgsWkbTypes.LineGeometry:
-                    geometry_type = "linestring"
-                wkt_geometry = self.getWktFromReferences(connection, geometry_type, references)
-
-                # Set the updated geometry to the feature
-                if wkt_geometry:
-                    qgis_geometry = QgsGeometry.fromWkt(wkt_geometry)
-                    output_feature.setGeometry(qgis_geometry)
-                else:
-                    unchanged_count += 1
-
-            else:
+            # Calculate references from the geometry
+            # We do it if update_policy is "update_references"
+            # and also for update_geometry
+            # if the parameter UPDATE_REFERENCES is set to yes
+            if update_policy == "update_references" or (
+                update_policy == "update_geometry" and preserve_start_end_positions == "yes"
+            ):
                 # We need to update the feature references from the feature geometry
                 if QgsWkbTypes.geometryType(source.wkbType()) == QgsWkbTypes.PointGeometry:
                     references_keys = ["road_code", "marker_code", "abscissa", "side", "offset", "cumulative"]
@@ -398,18 +401,18 @@ class UpdateManagedObjects(BaseProcessingAlgorithm):
                         unchanged_count += 1
                         continue
 
-                    # Get the updated geometry from the database for the point
+                    # Get the references from the database for the point
                     point = feature.geometry().asPoint()
-                    references = self.getReferencesFromLonLat(connection, point.x(), point.y())
+                    point_references = self.getReferencesFromLonLat(connection, point.x(), point.y())
                     has_changed = False
-                    if references:
-                        # print(f"references for {feature.id()} : {references}")
+                    if point_references:
+                        # print(f"references for {feature.id()} : {point_references}")
                         for ref in references_keys:
                             if ref not in feature.fields().names():
                                 continue
                             output_feature[ref] = feature[ref]
-                            if feature[ref] != references.get(ref):
-                                output_feature[ref] = references.get(ref)
+                            if feature[ref] != point_references.get(ref):
+                                output_feature[ref] = point_references.get(ref)
                                 has_changed = True
                     if not has_changed:
                         unchanged_count += 1
@@ -432,47 +435,114 @@ class UpdateManagedObjects(BaseProcessingAlgorithm):
                         last_vertex = feature.geometry().asMultiPolyline()[-1][-1]
 
                     # Result dictionary containing the updated references for the line start and end points
-                    result = {}
+                    line_references = {}
 
                     # Get the references from the database for the line start point
-                    result["start"] = self.getReferencesFromLonLat(
+                    line_references["start"] = self.getReferencesFromLonLat(
                         connection, first_vertex.x(), first_vertex.y()
                     )
-                    # print(f"start references for {feature.id()} : {result["start"]}")
+                    # print(f"start references for {feature.id()} : {line_references["start"]}")
 
                     # Get the references from the database for the line end point
-                    result["end"] = self.getReferencesFromLonLat(connection, last_vertex.x(), last_vertex.y())
-                    # print(f"end references for {feature.id()} : {result["end"]}")
+                    line_references["end"] = self.getReferencesFromLonLat(
+                        connection, last_vertex.x(), last_vertex.y()
+                    )
+                    # print(f"end references for {feature.id()} : {line_references["end"]}")
+
+                    # If the geometry orientation is wrong
+                    # (end point is at a lower reference than the start point)
+                    # we invert the start and end references
+                    if line_references["start"] and line_references["end"]:
+                        start_value = line_references["start"]["marker_code"] * 1000000
+                        start_value += line_references["start"]["abscissa"]
+                        end_value = line_references["end"]["marker_code"] * 1000000
+                        end_value += line_references["end"]["abscissa"]
+                        if start_value > end_value:
+                            start_references = line_references["start"]
+                            end_references = line_references["end"]
+                            line_references["start"] = end_references
+                            line_references["end"] = start_references
 
                     # Update feature attributes
                     has_changed = False
                     # road_code, offset & side of the start point
                     for attribute in ("road_code", "offset", "side"):
+                        # Do not change the references for the ouput feature
                         if attribute not in feature.fields().names():
                             continue
                         output_feature[attribute] = feature[attribute]
-                        if not result["start"]:
+                        if not line_references["start"]:
                             continue
-                        # print(f'{feature.id()} - {attribute} = {result["start"][attribute]}')
-                        if feature[attribute] != result["start"][attribute]:
-                            output_feature[attribute] = result["start"][attribute]
+                        # print(f'{feature.id()} - {attribute} = {line_references["start"][attribute]}')
+                        if feature[attribute] != line_references["start"][attribute]:
+                            output_feature[attribute] = line_references["start"][attribute]
                             has_changed = True
 
                     # start_ and end_ attributes
                     references_keys = ["marker_code", "abscissa", "cumulative"]
                     for place in ("start", "end"):
+                        # Do not change the references for the ouput feature
                         for ref in references_keys:
                             if f"{place}_{ref}" not in feature.fields().names():
                                 continue
                             output_feature[f"{place}_{ref}"] = feature[f"{place}_{ref}"]
-                            if not result[place]:
+                            if not line_references[place]:
                                 continue
-                            # print(f'{feature.id()} - {place}_{ref} = {result[place][ref]}')
-                            if feature[f"{place}_{ref}"] != result[place][ref]:
-                                output_feature[f"{place}_{ref}"] = result[place][ref]
+                            # print(f'{feature.id()} - {place}_{ref} = {line_references[place][ref]}')
+                            if feature[f"{place}_{ref}"] != line_references[place][ref]:
+                                output_feature[f"{place}_{ref}"] = line_references[place][ref]
                                 has_changed = True
                     if not has_changed:
                         unchanged_count += 1
+
+            # We need to update the feature geometry from the feature references
+            if update_policy == "update_geom":
+                # We get the references from the feature
+                # We use the output_feature since it could have been updated above
+                # if the update_policy was update_geom and preserve_start_end_positions == 'yes'
+                if QgsWkbTypes.geometryType(source.wkbType()) == QgsWkbTypes.PointGeometry:
+                    references_keys = ["road_code", "marker_code", "abscissa", "side", "offset"]
+                    references = {
+                        a: output_feature.attribute(a)
+                        for a in references_keys
+                        if a in output_feature.fields().names()
+                    }
+                else:
+                    references_keys = [
+                        "road_code",
+                        "start_marker_code",
+                        "start_abscissa",
+                        "end_marker_code",
+                        "end_abscissa",
+                        "side",
+                        "offset",
+                    ]
+                    references = {
+                        a: output_feature.attribute(a)
+                        for a in references_keys
+                        if a in output_feature.fields().names()
+                    }
+
+                # Set default values for side and offset if they are not present in the feature attributes
+                if "side" not in references:
+                    references["side"] = "right"
+                if "offset" not in references:
+                    references["offset"] = 0
+
+                # Get WKT geometry from the database
+                geometry_type = "point"
+                if QgsWkbTypes.geometryType(source.wkbType()) == QgsWkbTypes.LineGeometry:
+                    geometry_type = "linestring"
+
+                # Request the database for the updated geometry
+                wkt_geometry = self.getWktFromReferences(connection, geometry_type, references)
+
+                # Set the updated geometry to the feature
+                if wkt_geometry:
+                    qgis_geometry = QgsGeometry.fromWkt(wkt_geometry)
+                    output_feature.setGeometry(qgis_geometry)
+                else:
+                    unchanged_count += 1
 
             # Add the feature to the output layer sink
             sink.addFeature(output_feature, QgsFeatureSink.FastInsert)
